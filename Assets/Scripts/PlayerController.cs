d3639e1c0a5ae87ac2c73fd83e9655d6c89636f7
Rajat <makegamewithrajat@gmail.com>
2022-07-03 23:23:54 +0530

    Player movement
    implement the Player Control and  and level win trigger, adding Door and fake doors

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0)//positive horizontal
+        {
+            rigidbody2d.velocity = new Vector2(speed, 0f);
+        }else if(Input.GetAxis("Horizontal") < 0)//negative horizontal
+        {
+            rigidbody2d.velocity = new Vector2(-speed, 0f);   
+        }else if(Input.GetAxis("Vertical") > 0)//positive vertical
+        {
+           rigidbody2d.velocity = new Vector2(0f, speed); 
+        }else if(Input.GetAxis("Vertical") < 0)// negative vertical
+        {
+            rigidbody2d.velocity = new Vector2(0f, -speed);
+        }else if(Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            //stop
+            rigidbody2d.velocity = new Vector2(0f, 0f);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Door")
+            Debug.Log("Level Completed !!");        
+    }
+}
