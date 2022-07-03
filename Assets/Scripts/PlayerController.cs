6cff64b574b6852866a16d834eb1269c095664c7
Rajat <makegamewithrajat@gmail.com>
2022-07-04 00:20:46 +0530

    Added UI in game
    added UI in game as well as imported textMesh Pro

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidbody2d;
 
+    public GameObject gamewon;
+
     public float speed;
+
+    private bool isgamewon = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isgamewon == true)
+        {
+            return;
+        }
+        
         if(Input.GetAxis("Horizontal") > 0)//positive horizontal
         {
             rigidbody2d.velocity = new Vector2(speed, 0f);
@@ -39,6 +48,11 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Door")
-            Debug.Log("Level Completed !!");        
+        {
+            Debug.Log("Level Completed !!");
+            gamewon.SetActive(true);
+            isgamewon = true;
+        }
+                    
     }
 }
